5e51b7159ace41e5804d130c872243223a49c92d
YagoHD <yagorb@hotmail.com>
2022-11-03 16:11:04 +0100

    ADD: Funcionamiento Game Over-> Play Again. Ahora podemos jugar de nuevo al perder una partida, clicando en el botón de play again.

--- app/src/main/java/com/example/divingdolphin/GameOver.java
+++ app/src/main/java/com/example/divingdolphin/GameOver.java
@@ -2,13 +2,31 @@ package com.example.divingdolphin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameOver extends AppCompatActivity {
+    //Boton de jugar de nuevo
+    private Button empezarJuego;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        empezarJuego = (Button) findViewById(R.id.playAgain);
+
+        empezarJuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent (GameOver.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
