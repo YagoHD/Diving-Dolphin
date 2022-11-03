10d1820411675939c298824a1897a005f59d1a3b
YagoHD <yagorb@hotmail.com>
2022-11-03 11:23:42 +0100

    ADD: Movimiento del delfín. Se añadió un método click, con el cual podemos hacer que el delfín suba, también se la añadió gravedad al delfín para que baje solo, se limitaron los topes del suelo y la altura, y se le crearon los siguientes atributos al personaje: velocidad y altura.

--- app/src/main/java/com/example/divingdolphin/Delfin.java
+++ app/src/main/java/com/example/divingdolphin/Delfin.java
@@ -7,23 +7,33 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class Delfin extends View {
     //Creamos el Bitmap Delfin
-    private Bitmap delfin;
+    private Bitmap delfin[] = new Bitmap[2];
+    private int delfinX = 10;
+    private int delfinY;
+    private int velocidadDelfin;
+
+    private int canvasWidth, canvasHeight;
     //Creamos el fondo del nivel
     private Bitmap imagenDeFondo;
     //Creamos el marcador de puntuacion
     private Paint marcadorPuntuacion = new Paint();
     //Creamos la vida del personaje
     private Bitmap vida[] = new Bitmap[2];
+    //Creacion del toque en la pantalla
+    private boolean click = false;
+
 
     public Delfin(Context context) {
         super(context);
 
         //Instanciamos el bitmap del delfin
-        delfin = BitmapFactory.decodeResource(getResources(), R.drawable.delfinarriba);
+        delfin[0] = BitmapFactory.decodeResource(getResources(), R.drawable.delfinarriba);
+        delfin[1] = BitmapFactory.decodeResource(getResources(), R.drawable.delfinabajo);
         //Instanciamos la imagen de fondo
         imagenDeFondo = BitmapFactory.decodeResource(getResources(), R.drawable.fondo1);
         //Instanciamos el marcador de puntuacion
@@ -34,23 +44,52 @@ public class Delfin extends View {
         //Instanciamos la vida
         vida[0] = BitmapFactory.decodeResource(getResources(), R.drawable.corazonredimensionado);
         vida[1] = BitmapFactory.decodeResource(getResources(), R.drawable.corazonroto2);
-
-
+        //Punto de partida del delfin
+        delfinY = 550;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
         //Añadimos la imagen de fondo a la pantalla
         canvas.drawBitmap(imagenDeFondo,0,0,null);
-        //Añadimos el delfin a la pantalla
-        canvas.drawBitmap(delfin,0,0, null);
+        //Añadimos y posidionamos el delfin a la pantalla
+        int minDelfinY = delfin[0].getHeight();
+        int maxDelfinY = canvasHeight - delfin[0].getHeight() * 3;
+        delfinY = delfinY + velocidadDelfin;
+
+        if (delfinY < minDelfinY){
+            delfinY = minDelfinY;
+        }
+        if (delfinY > maxDelfinY){
+            delfinY = maxDelfinY;
+        }
+        velocidadDelfin = velocidadDelfin + 2;
+        if (click){
+            canvas.drawBitmap(delfin[1], delfinX,delfinY, null);
+            click = false;
+        }else{
+        canvas.drawBitmap(delfin[0], delfinX, delfinY, null);
+        }
+
         //Añadimos los corazones y la puntuacion en la pantalla
         canvas.drawText("Puntuación: ", 20, 100, marcadorPuntuacion);
         canvas.drawBitmap(vida[0], 600, 35, null);
         canvas.drawBitmap(vida[0], 700, 35, null);
         canvas.drawBitmap(vida[0], 800, 35, null);
+    }
 
-
+    //Creamos el evento de cuando clicamos en pantalla y el delfin suba
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            click = true;
+            velocidadDelfin = -21;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/divingdolphin/MainActivity.java
+++ app/src/main/java/com/example/divingdolphin/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.divingdolphin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     //Creamos el personaje que tendra la funcion del delfin
     private Delfin personajeDelfin;
+    private Handler handler = new Handler();
+    private final static long Interval = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
         personajeDelfin = new Delfin(this);
         setContentView(personajeDelfin);
+
+        //Hacemos que el delfin se caiga cuando no se pulsa el click
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        personajeDelfin.invalidate();
+                    }
+                });
+            }
+        },0, Interval);
     }
 }
